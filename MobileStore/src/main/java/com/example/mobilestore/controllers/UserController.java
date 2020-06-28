8b932087c93c2e6c8b065a6e96b3fc84b71aaf6c
beobanhbo <45838508+beobanhbo@users.noreply.github.com>
2020-06-28 22:43:53 +0700

    doi ten csdl, routing trong bang USER

--- MobileStore/src/main/java/com/example/mobilestore/controllers/UserController.java
+++ MobileStore/src/main/java/com/example/mobilestore/controllers/UserController.java
@@ -3,14 +3,18 @@ package com.example.mobilestore.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.mobilestore.repository.UserRepository;
 import com.example.mobilestore.entities.User;
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("user")
 public class UserController {
 	@Autowired
 	private UserRepository userRepository;
@@ -18,5 +22,16 @@ public class UserController {
 	@GetMapping("/list")
 	 public List<User> getAllUsers() {
 	 return userRepository.findAll();
+	 }
+	@PostMapping("/signin")
+	 public ResponseEntity<User> signIn(@Validated @RequestBody User u) {
+	 User user = userRepository.findByNameAndPassword(u.getName(), 
+	u.getPassword());
+	
+	 if (user == null) {
+	 return ResponseEntity.ok(null);
+	 }
+	
+	 return ResponseEntity.ok(user);
 	 }
 }
--- MobileStore/src/main/java/com/example/mobilestore/repository/UserRepository.java
+++ MobileStore/src/main/java/com/example/mobilestore/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.example.mobilestore.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.mobilestore.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Integer>{
 	
-	public User findByUsernameAndPassword(String username, String password);
+	public User findByNameAndPassword(String user, String password);
+//	public User getAllUsers(String name, String password);
+	public List<User> findAll();
 }
